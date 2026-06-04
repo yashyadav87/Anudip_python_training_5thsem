96d29a573d367d0ca1f1a17820cde724cb5b34fb
yashyadav87 <yashyadav7914@gmail.com>
2026-06-04 11:59:33 +0530

    Create types_of_triangle.py

---
+++ self_learning_3rd june/types_of_triangle.py
@@ -0,0 +1,18 @@
+#wap to specify the type of triangle,if three sides form a triangle
+a = float(input("Enter first side: "))
+b = float(input("Enter second side: "))
+c = float(input("Enter third side: "))
+
+if a <= 0 or b <= 0 or c <= 0:
+    print("Invalid input! Sides must be positive.")
+elif a + b > c and a + c > b and b + c > a:
+    print("Valid Triangle")
+
+    if a == b == c:
+        print("Type: Equilateral Triangle")
+    elif a == b or b == c or a == c:
+        print("Type: Isosceles Triangle")
+    else:
+        print("Type: Scalene Triangle")
+else:
+    print("Not a Triangle")
