a9b4de9295631f6c035f28206c3672650ab4f5a9
yashyadav87 <yashyadav7914@gmail.com>
2026-06-04 12:18:20 +0530

    Create attendence_count.py

---
+++ classwork/iteration_4th june/attendence_count.py
@@ -0,0 +1,13 @@
+#attendancecounter
+student = int(input("Enter the number of students in the class: "))
+attendance = 0
+if student < 0:
+    exit("Invalid input. Number of students cannot be negative.")
+    students = 0 
+    print(students)
+    while(attendance <= student):
+        if(attendance):
+            print("Attendance count:", attendance)
+            attendance += 1
+        else:
+            break
